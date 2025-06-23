01609264cf27442d62a0ce37ff3160bb6950848b
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2025-06-23 10:38:23 +0900

    [Bronze V] Title: 고양이, Time: 92 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+//  꼬마 정민
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" + " )  ( ')\n" + "(  /  )\n" + " \\(__)|");
+    }
+}
