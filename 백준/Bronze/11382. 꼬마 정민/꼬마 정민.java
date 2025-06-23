6a2c8084c809d7b47b00017610d7e7c10410755f
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2025-06-23 10:35:30 +0900

    [Bronze V] Title: 꼬마 정민, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+//  꼬마 정민
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        System.out.println(A+B+C);
+    }
+}
