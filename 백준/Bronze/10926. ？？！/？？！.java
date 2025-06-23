1f74899d0a12c3a378e1ecc78207454dd49db7df
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2025-06-23 10:12:58 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+//  ??!
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String userId = sc.nextLine();
+        System.out.println(userId + "??!");
+    }
+}
