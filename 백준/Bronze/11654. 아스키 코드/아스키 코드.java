1e09cc47ccc187484aec75b6388c8eef3006f8d3
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2025-06-27 11:43:26 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int) sc.nextLine().charAt(0));
+    }
+}
