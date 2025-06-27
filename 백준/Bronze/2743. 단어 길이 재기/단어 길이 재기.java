116a351ba59b9b467ba03ae645c19e54103ffb46
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2025-06-27 11:18:39 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input.length());
+    }
+}
