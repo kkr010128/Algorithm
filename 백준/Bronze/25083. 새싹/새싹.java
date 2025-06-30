d8abd09e6c1afd3ddda718712f72161ab01b1c2a
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2025-06-30 11:21:07 +0900

    [Bronze V] Title: 새싹, Time: 108 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+
+    }
+}
