b10a4e491a659013f193f0f3826ff898f0e0ded5
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2025-07-03 10:19:22 +0900

    [Silver V] Title: 너의 평점은, Time: 112 ms, Memory: 14632 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Double> gradeMap = new HashMap<>();
+        gradeMap.put("A+", 4.5);
+        gradeMap.put("A0", 4.0);
+        gradeMap.put("B+", 3.5);
+        gradeMap.put("B0", 3.0);
+        gradeMap.put("C+", 2.5);
+        gradeMap.put("C0", 2.0);
+        gradeMap.put("D+", 1.5);
+        gradeMap.put("D0", 1.0);
+        gradeMap.put("F", 0.0);
+
+        double totalScore = 0.0;
+        double totalCredit = 0.0;
+
+        for (int i = 0; i < 20; i++) {
+            String[] input = br.readLine().split(" ");
+            String subject = input[0];
+            double credit = Double.parseDouble(input[1]);
+            String grade = input[2];
+
+            if (!grade.equals("P")) {
+                totalCredit += credit;
+                totalScore += credit * gradeMap.get(grade);
+            }
+        }
+
+        System.out.printf("%.6f", totalScore / totalCredit);
+    }
+}
