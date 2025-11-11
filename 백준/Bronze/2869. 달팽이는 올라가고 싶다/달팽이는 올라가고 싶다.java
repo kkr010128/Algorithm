991caf12cdc3c0d252705650069aa4913df69cab
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2025-11-11 14:51:39 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        br.close();
+        int day = (int) Math.ceil((double) (v - b) / (a - b));
+        System.out.println(day);
+    }
+}
