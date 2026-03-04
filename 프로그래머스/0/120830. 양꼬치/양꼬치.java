05334484a438b074a672f5b017b768920aadaa88
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2026-03-04 22:37:10 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(int n, int k) {
-        int lambPrice = n * 12000;
-        int freeDrink = n / 10;
-        int drinkPrice = (k - freeDrink) * 2000;
-        
-        return lambPrice + drinkPrice;
+        int answer = 0;
+        return answer;
     }
 }
