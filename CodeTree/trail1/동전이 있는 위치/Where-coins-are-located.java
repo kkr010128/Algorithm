a45ec897f433fc6ba97b2b0e8744712c9553470e
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2026-07-30 19:31:12 +0900

    [Passed] 동전이 있는 위치 (135ms, 10MB)

---
+++ CodeTree/trail1/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt(), M = sc.nextInt(); 
+        int [][] arr = new int [N+1][N+1];  
+        int row, col;
+
+
+        for (int i = 0; i < M; i++) {
+            row = sc.nextInt(); 
+            col = sc.nextInt(); 
+            arr[row][col] = 1;
+        }
+        
+        
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                System.out.printf("%d ", arr[i][j]);
+            }
+            System.out.println();
+        }
+        
+    }
+}
