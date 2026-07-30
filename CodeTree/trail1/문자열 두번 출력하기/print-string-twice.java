74ed177070ab62ce1061f06ff7e4be6710e8c456
Kim Kwangrae <74182629+kkr010128@users.noreply.github.com>
2026-07-31 08:47:10 +0900

    [Passed] 문자열 두번 출력하기 (124ms, 10MB)

---
+++ CodeTree/trail1/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.next();
+        System.out.printf("%s\n%s", line, line);
+    }
+}
