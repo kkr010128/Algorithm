50410e57ebc8b3fccbe3691a4184d83015a4ad8c
kkr010128 <74182629+kkr010128@users.noreply.github.com>
2026-08-15 23:02:08 +0900

    [Passed] 변수 값 교체하기 3 (117ms, 20MB)

---
+++ CodeTree/trail1/변수 값 교체하기 3/replacing-variable-values-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3, b = 5;
+        int tmp = a;
+        a = b;
+        b = tmp;
+        System.out.printf("%d\n%d", a, b);
+    }
+}
