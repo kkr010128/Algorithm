095d8d3e3e32e53caf4ae56cb1bd1b7dfaff0bac
kkr010128 <74182629+kkr010128@users.noreply.github.com>
2026-08-20 21:46:38 +0900

    [Passed] 길이 단위 환산하기 (148ms, 11MB)

---
+++ CodeTree/trail1/길이 단위 환산하기/convert-length-units.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double ft = sc.nextDouble();
+        double cm = ft * 30.48;
+
+        System.out.printf("%.1f", cm);
+    }
+}
