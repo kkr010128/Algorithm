4199c0fbd56253450f2b10db86eeddfc3dce6805
kkr010128 <74182629+kkr010128@users.noreply.github.com>
2026-08-22 20:02:38 +0900

    [Passed] 정수 세 개 입력받아 출력 (132ms, 10MB)

---
+++ CodeTree/trail1/정수 세 개 입력받아 출력/take-three-integers-and-output.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d %d %d", sc.nextInt(), sc.nextInt(), sc.nextInt());
+    }
+}
