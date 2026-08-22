b204007b36e1b90fcd7efed0c35f268b6edeb10d
kkr010128 <74182629+kkr010128@users.noreply.github.com>
2026-08-22 20:09:09 +0900

    [Passed] 문자열 순서 바꾸기 (126ms, 10MB)

---
+++ CodeTree/trail1/문자열 순서 바꾸기/change-order-of-strings.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String tmp = sc.next();
+        System.out.printf("%s\n%s", sc.next(), tmp);
+    }
+}
