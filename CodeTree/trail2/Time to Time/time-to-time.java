6608257d2735d3e34fdf47e69c64c752485c6968
kkr010128 <74182629+kkr010128@users.noreply.github.com>
2026-08-25 13:53:08 +0900

    [Passed] Time to Time (135ms, 10MB)

--- CodeTree/trail2/Time to Time/time-to-time.java
+++ CodeTree/trail2/Time to Time/time-to-time.java
@@ -3,9 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
-        int from = a * 60 + b;
-        int to = c * 60 + d;
-        System.out.print(to-from);
+        System.out.print(-1 * ((60 * sc.nextInt() + sc.nextInt()) - (60 * sc.nextInt() + sc.nextInt())));
     }
 }
