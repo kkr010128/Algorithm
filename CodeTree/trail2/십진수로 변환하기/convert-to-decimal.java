64b8cbb610efa38128db0ad70479b8f983fbf2a0
kkr010128 <74182629+kkr010128@users.noreply.github.com>
2026-08-28 16:08:07 +0900

    [Passed] 십진수로 변환하기 (126ms, 10MB)

--- CodeTree/trail2/십진수로 변환하기/convert-to-decimal.java
+++ CodeTree/trail2/십진수로 변환하기/convert-to-decimal.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int result = 0;
         int num = sc.nextInt();
+        int result = 0;
         int cnt = 1;
-        
-        while (num > 0) {
-            int mod = num % 10;
-            result += mod * cnt;
+
+        while(num > 0) {
+            result += cnt * (num % 10);
             cnt *= 2;
-            num = num/10;
+            num = num / 10;
         }
         System.out.print(result);
     }
