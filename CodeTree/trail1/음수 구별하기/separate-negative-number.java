9b58aeead7e96167be7ecff05b497c970079edba
kkr010128 <74182629+kkr010128@users.noreply.github.com>
2026-09-18 17:13:14 +0900

    [Passed] 음수 구별하기 (134ms, 10MB)

--- CodeTree/trail1/음수 구별하기/separate-negative-number.java
+++ CodeTree/trail1/음수 구별하기/separate-negative-number.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num  = sc.nextInt(); 
-        System.out.print(num<0 ? num+"\nminus" :num);
+
+        int n = sc.nextInt();
+
+        System.out.println(n);
+
+        if (n < 0) {
+            System.out.println("minus");
+        }
     }
 }
